3fce16a167cf9786d32a63305786261db446c96e
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-10-01 09:53:08 +0500

    file

--- file01.py
+++ file01.py
@@ -1,4 +1,7 @@
 def main(data:str):
+    f=open('data01.txt')
+    x=f.read()
+    x=x.split()
     """
     The data is from the file. Return data as a list type.
     Args:
@@ -6,5 +9,6 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    return x
 
 # Read data from file
--- file02.py
+++ file02.py
@@ -1,4 +1,7 @@
 def main(data:str):
+    f=open('data02.txt')
+    x=f.read()
+    return len(x)
     """
     The data is from the file. Return number of characters in the file.
     Args:
